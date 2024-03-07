5c6fe51d617e1417b37927a8fbf088cccfddde6c
Ideli Zanesco <115120394+idyzf@users.noreply.github.com>
2024-03-07 11:35:24 -0300

    Update import pandas as pd.py

--- import pandas as pd.py
+++ import pandas as pd.py
@@ -3,7 +3,6 @@ import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
 import plotly.io as pio
-
 pio.templates.default = "plotly_white"
 
 data = pd.read_csv("Ratio_teste_ML.csv", delimiter=";")
